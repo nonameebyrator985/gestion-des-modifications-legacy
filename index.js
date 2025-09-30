f7221223bd435861c55d84b187b3b3acd9bf6cd7
nonameebyrator985 <singheingemo@hotmail.com>
2025-09-30 23:38:49 +0900

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue dans l outil de gestion des modifications héritées!');
+});
+
+const server = app.listen(port, () => {
+  console.log(`Serveur en écoute sur http://localhost:${port}`);
+});
+
+module.exports = server;
